f49f5754a0e6456b5541b1d72a8a2e30be8eccb0
nthfvr <nathan.favry@efrei.net>
2023-11-26 22:59:34 +0100

    Ajout de la fonction IDF

--- functions.py
+++ functions.py
@@ -120,3 +120,54 @@ def tf(chaine):
 
 
     return dictionnaire_mot
+
+
+import math
+
+
+# Fonction qui calcule le score IDF pour chaque mot dans un répertoire de fichiers texte
+def IDF(repertoire):
+    # Récupération de la liste des noms de fichiers texte dans le répertoire
+    files_names = list_of_files(repertoire, "txt")
+    # Initialisation d'un ensemble pour stocker tous les mots uniques dans les documents
+    set_mots = set([])
+    # Initialisation du nombre total de documents
+    nb_doc = 0
+    # Boucle pour parcourir chaque fichier dans le répertoire
+    for names in files_names:
+        # Lecture du contenu du fichier
+        with open(repertoire + names, "r", encoding="utf-8") as fichier_cleaned:
+            contenu = fichier_cleaned.read()
+            # Division du contenu en une liste de mots
+            liste_de_mots = contenu.split()
+            # Ajout de chaque mot à l'ensemble des mots uniques
+            for mots in liste_de_mots:
+                set_mots.add(mots)
+            # Incrémentation du nombre de documents
+            nb_doc += 1
+    # Initialisation d'un dictionnaire pour stocker le nombre d'occurrences de chaque mot dans les documents
+    dico_occurence_mot = {}
+    # Boucle pour chaque mot unique dans l'ensemble de mots
+    for mots in set_mots:
+        # Initialisation du compteur d'occurrences pour le mot en cours
+        cpt_occu_doc = 0
+        # Boucle pour chaque fichier dans le répertoire
+        for names in files_names:
+            # Lecture du contenu du fichier
+            with open(repertoire + names, "r", encoding="utf-8") as fichier_cleaned:
+                contenu = fichier_cleaned.read()
+                # Division du contenu en une liste de mots
+                liste_de_mots = contenu.split()
+                # Vérification de l'occurrence du mot dans le fichier
+                if mots in liste_de_mots:
+                    cpt_occu_doc += 1
+        # Stockage du nombre d'occurrences du mot dans le dictionnaire
+        dico_occurence_mot[mots] = cpt_occu_doc
+    # Initialisation d'un dictionnaire pour stocker le score IDF de chaque mot
+    score_idf = {}
+    # Boucle pour chaque mot dans le dictionnaire d'occurrences
+    for cle in dico_occurence_mot.keys():
+        # Calcul du score IDF et stockage dans le dictionnaire
+        score_idf[cle] = math.log10((nb_doc / (dico_occurence_mot[cle]+1)))
+    # Retourne le dictionnaire final des scores IDF pour chaque mot
+    return score_idf
