c91d1b8c9c5ce048a2315832de3653272ae3cc6f
Babouzi <150954596+Babouzi@users.noreply.github.com>
2023-12-19 01:08:04 +0100

    En tête du fichier main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""Projet Chatbot;
+Auteurs : Nathan Favry , Eham Bill Abouzi
+Ce fichier contient le programme principal. Il suffit de le lancer pour accéder aux fonctionnalités.
+"""
+
 import functions
 
 if __name__ == '__main__':
